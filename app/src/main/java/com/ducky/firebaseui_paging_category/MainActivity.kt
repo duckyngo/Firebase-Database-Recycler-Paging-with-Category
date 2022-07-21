1aceb34c2263343dd6992d101d0fbbf163f65396
Duc Ky <22850962+duckyngo@users.noreply.github.com>
2022-07-21 16:18:36 +0900

    Create Android project

---
+++ app/src/main/java/com/ducky/firebaseui_paging_category/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ducky.firebaseui_paging_category
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
